ade7d23abc80e190e5045d9d5d73436d466bc622
Jayant-Deshmukh <37954829+Jayant-Deshmukh@users.noreply.github.com>
2018-06-10 12:37:12 +0530

    Update and rename liquid.mk to aosp_land.mk

--- aosp_land.mk
+++ aosp_land.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Validus stuff
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,7 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 3S
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Wingtech
-PRODUCT_NAME := liquid_land
+PRODUCT_NAME := aosp_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -41,6 +41,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/land/land:6.0.1/MMB29M/V9.5.1.0.MALMIFA:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Redmi 3s" \
     DEVICE_MAINTAINERS="Jayant-Deshmukh"
 	
